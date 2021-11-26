a463eb29bf0df6537ff2dcc7231350f1ca1d0131
Cristian <cristiancuello10@gmail.com>
2021-11-26 15:41:35 -0300

    env fish

--- reservas-ui/src/context/authContext/utils.js
+++ reservas-ui/src/context/authContext/utils.js
@@ -1,17 +1,13 @@
-// import CryptoJS from "crypto-js";
+import CryptoJS from "crypto-js";
 
-// export const encryptData = (data, salt) =>
-//   CryptoJS.AES.encrypt(JSON.stringify(data), salt).toString();
+export const encryptData = (data, salt) =>
+  CryptoJS.AES.encrypt(JSON.stringify(data), salt).toString();
 
-// export const decryptData = (ciphertext, salt) => {
-//   const bytes = CryptoJS.AES.decrypt(ciphertext, salt);
-//   try {
-//     return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
-//   } catch (e) {
-//     return null;
-//   }
-// };
-//
-
-export const encryptData=(data,salt)=> console.log("HOLA")
-export const decryptData=(c,s)=> console.log("Dec")
+export const decryptData = (ciphertext, salt) => {
+  const bytes = CryptoJS.AES.decrypt(ciphertext, salt);
+  try {
+    return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+  } catch (e) {
+    return null;
+  }
+};
